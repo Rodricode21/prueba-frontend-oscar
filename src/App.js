d25473b0100aaa62d8f597600ed761265935bc23
rodricode21 <kiradex21@gmail.com>
2023-07-29 13:31:17 -0600

    changed to english

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 import MovieForm from "./components/MovieForm";
 
 function App() {
   return (
     <div>
-      <h1 className='header'>Películas</h1>
+      <h1 className="header">Movies</h1>
       <MovieForm />
     </div>
   );
