f74b0830d19610b13642e1668cec3e03d6a4a827
rodricode21 <kiradex21@gmail.com>
2023-07-31 07:08:45 -0600

    adding react-router to my project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import MovieForm from "./components/MovieForm";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+
+import { PathRoutes } from "./routes/routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="header">Movies</h1>
-      <MovieForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={PathRoutes.Home} element={<Home />} />
+        <Route path={PathRoutes.Form} element={<MovieForm />} />
+        <Route path={PathRoutes.Movies} element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
